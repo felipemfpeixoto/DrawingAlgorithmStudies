86faccfbac0965fa5f203edd6f3ee2433097c1c0
Felipe Peixoto <felipe.m.f.peixoto@gmail.com>
2025-11-19 16:29:00 -0300

    Initial Commit

---
+++ DrawingAlgorithmStudies/DrawingAlgorithmStudiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAlgorithmStudiesApp.swift
+//  DrawingAlgorithmStudies
+//
+//  Created by Felipe Mussi Ferreira Peixoto on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DrawingAlgorithmStudiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrawingAlgorithmStudiesTests/DrawingAlgorithmStudiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingAlgorithmStudiesTests.swift
+//  DrawingAlgorithmStudiesTests
+//
+//  Created by Felipe Mussi Ferreira Peixoto on 19/11/25.
+//
+
+import Testing
+@testable import DrawingAlgorithmStudies
+
+struct DrawingAlgorithmStudiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
